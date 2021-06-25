14d7e180a1f66b0ef70b0bf2b763016aec7bb1ff
Luke Vo <datvm@outlook.com>
2021-06-25 10:11:41 +0700

    Added Oxygen Plant

---
+++ LukeMods.OxygenPlant/Config.cs
@@ -0,0 +1,22 @@
+﻿using LukeMods.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LukeMods.OxygenPlant
+{
+    public class Config : BaseConfig
+    {
+        public static readonly Config Instance = new Config();
+
+        public float Duration { get; set; } = 60f;
+        public float Capacity { get; set; } = 70f;
+
+        private Config()
+        {
+        }
+
+    }
+}
---
+++ LukeMods.OxygenPlant/OxygenPlantPatches.cs
@@ -0,0 +1,39 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using OP = global::OxygenPlant;
+
+namespace LukeMods.OxygenPlant
+{
+    [HarmonyPatch(typeof(OP))]
+    public static class OxygenPlantPatches
+    {
+
+        static readonly Config C = Config.Instance;
+
+        [HarmonyPatch(nameof(GetProgress)), HarmonyPrefix]
+        public static void GetProgress(OP __instance)
+        {
+            SetValues(__instance);
+        }
+
+        [HarmonyPatch(nameof(OnHandClick)), HarmonyPrefix]
+        public static void OnHandClick(OP __instance)
+        {
+            SetValues(__instance);
+        }
+
+        static void SetValues(OP __instance)
+        {
+            var c = C;
+            __instance.duration = c.Duration;
+            __instance.capacity = c.Capacity;
+        }
+
+    }
+
+}
