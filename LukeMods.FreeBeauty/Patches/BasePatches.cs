70574cf29621a57191bfef3c0c5e8273e5b8f1d8
Luke Vo <datvm@outlook.com>
2021-06-26 15:08:29 +0700

    Added Free Glass.

---
+++ LukeMods.FreeBeauty/Patches/BasePatches.cs
@@ -0,0 +1,93 @@
+﻿using HarmonyLib;
+using LukeMods.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LukeMods.FreeBeauty.Patches
+{
+    [HarmonyPatch(typeof(Base))]
+    public static class BasePatches
+    {
+        static readonly FieldInfo FaceHullStrength = AccessTools.Field(typeof(Base), nameof(FaceHullStrength));
+        static readonly FieldInfo CellHullStrength = AccessTools.Field(typeof(Base), nameof(CellHullStrength));
+
+        static bool patched = false;
+
+        static readonly Dictionary<TechType, float> FaceHullStrReplacement = new Dictionary<TechType, float>()
+        {
+            { TechType.BaseWindow, 0 },
+            { TechType.BaseGlassDome, 0 },
+            { TechType.BaseLargeGlassDome, 0 },
+        };
+
+        static readonly Dictionary<Base.CellType, float> CellHullStrReplacement = new Dictionary<Base.CellType, float>()
+        {
+            { Base.CellType.Observatory, -1.25f },
+        };
+
+        [HarmonyPatch(nameof(Awake)), HarmonyPostfix]
+        public static void Awake()
+        {
+            if (patched)
+            {
+                return;
+            }
+
+            patched = true;
+
+            // Face
+            var facesStr = FaceHullStrength.GetValue(null) as float[];
+            var faces = Base.FaceToRecipe;
+            var len = faces.Length;
+            for (int i = 0; i < len; i++)
+            {
+                if (FaceHullStrReplacement.TryGetValue(faces[i], out var str))
+                {
+                    facesStr[i] = str;
+                }
+            }
+
+            // Cell
+            var cellsStr = CellHullStrength.GetValue(null) as float[];
+            foreach (var cell in CellHullStrReplacement)
+            {
+                cellsStr[(int)cell.Key] = cell.Value;
+            }
+
+
+        }
+
+        [HarmonyPatch(nameof(GetHullStrength)), HarmonyTranspiler]
+        public static IEnumerable<CodeInstruction> GetHullStrength(IEnumerable<CodeInstruction> instructions)
+        {
+            var isGlass = false;
+            
+            foreach (var instruction in instructions)
+            {
+                if (instruction.opcode == OpCodes.Ldfld)
+                {
+                    var field = instruction.operand as FieldInfo;
+                    if (field.Name == "isGlass")
+                    {
+                        isGlass = true;
+                    }
+                }
+                else if (isGlass && (instruction.opcode == OpCodes.Brfalse || instruction.opcode == OpCodes.Brfalse_S))
+                {
+                    isGlass = false;
+
+                    yield return new CodeInstruction(OpCodes.Pop);
+                    yield return new CodeInstruction(OpCodes.Ldc_I4_0);
+                }
+
+                yield return instruction;
+            }
+        }
+
+    }
+}
---
+++ LukeMods.FreeBeauty/Patches/TechDataPatches.cs
@@ -0,0 +1,68 @@
+﻿using HarmonyLib;
+using LukeMods.Common;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace LukeMods.FreeBeauty.Patches
+{
+
+    [HarmonyPatch(typeof(TechData))]
+    public static class TechDataPatches
+    {
+        static readonly FieldInfo entries = AccessTools.Field(typeof(TechData), nameof(entries));
+        static readonly FieldInfo propertyToID = AccessTools.Field(typeof(TechData), nameof(propertyToID));
+
+        static readonly int propertyIngredients = (int)AccessTools.Field(typeof(TechData), nameof(propertyIngredients)).GetValue(null);
+        static readonly int propertyTechType = (int)AccessTools.Field(typeof(TechData), nameof(propertyTechType)).GetValue(null);
+        static readonly int propertyAmount = (int)AccessTools.Field(typeof(TechData), nameof(propertyAmount)).GetValue(null);
+
+        static readonly Dictionary<TechType, TechType> IngredientReplacements = new Dictionary<TechType, TechType>()
+        {
+            { TechType.BaseCorridorGlassI, TechType.BaseCorridorI },
+            { TechType.BaseCorridorGlassL, TechType.BaseCorridorL },
+            { TechType.BaseObservatory, TechType.BaseRoom },
+
+            { TechType.BaseGlassDome, TechType.BaseCorridorI },
+            { TechType.BaseLargeGlassDome, TechType.BaseCorridorI },
+            { TechType.BaseWindow, TechType.BaseCorridorI },
+        };
+
+        [HarmonyPatch(nameof(Cache)), HarmonyPrefix]
+        public static void Cache()
+        {
+            var e = entries.GetValue(null) as Dictionary<TechType, JsonValue>;
+
+            var ingredientsId = propertyIngredients;
+            foreach (var replacement in IngredientReplacements)
+            {
+                if (!e.TryGetValue(replacement.Key, out var src))
+                {
+                    continue;
+                }
+
+                if (!e.TryGetValue(replacement.Value, out var dest))
+                {
+                    continue;
+                }
+
+                src[ingredientsId] = dest[ingredientsId].Copy();
+            }
+        }
+
+        static IEnumerable<KeyValuePair<TKey, TValue>> ToEnumerable<TKey, TValue>(IEnumerator<KeyValuePair<TKey, TValue>> enumerator)
+        {
+            while (enumerator.MoveNext())
+            {
+                yield return enumerator.Current;
+            }
+        }
+
+    }
+
+}
