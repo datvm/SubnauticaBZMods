ef47618fd5a0299b34d52ccd976231f1b89c78e0
Maikel Finck <maikel-finck@users.noreply.github.com>
2021-10-21 22:59:00 -0300

    Improving the player experience and bug fixes.
    - Added the SMLHelper library to refactor the options solution. This will allow automatic manipulation of the config.json file and also automatically add the desired configuration in the Options menu under the Mods tab. Min, max and default values were set for GUI usage. Also, the way we recover the instance changed in the Config class but not in how it was used elsewhere.
    
    - Added visual feedback similar to the water filtration that will give the player feedback of the current status of the interest in the container the player is looking (has the cursor over the container). If the interest rate is not working or the container is full, it will display a message to "open" the container. If the interest rate is in progress, it will display a percentage value updated in real-time while keeping the cursor over the container. The different time required to the next interest payment will be noticeable as the percentage is faster or slower depending on how many items there is. Added a "ready" message when the current interest is done. The player still needs to open the container to compute the interest and create the item.
    
    - Fixed an issue that we needed to manually open a container to start the interest rate calculation after loading the game.
    
    - Fixed an issue that we needed to open, close, and open again a container to properly restart the interest rate calculation, otherwise that container would be left  hanging without the interest rate.
    
    - Fixed an issue that adding or removing items from the container would stop the current interest calculation and left it hanging. We needed to open, close, and open the container again to restart, similar to the previous fix.
    
    - Fixed an issue that when the interest rate is done and the player interacts to receive the item, the next interest cycle would not start until the player reopen the container.

--- LukeMods.StorageInterest/Initializer.cs
+++ LukeMods.StorageInterest/Initializer.cs
@@ -5,18 +5,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SMLHelper.V2.Handlers;
 
 namespace LukeMods.StorageInterest
 {
     [QModCore]
     public static class Initializer
     {
+        internal static Config config { get; private set; }
 
         [QModPatch]
         public static void Init()
         {
             BaseInitializer.Init(nameof(StorageInterest), typeof(Initializer).Assembly);
-            _ = Config.Instance;
+
+            // Use the SMLHelper config handler to automatic config.json IO and GUI options.
+            // This will generate the config.json file if missing and whenever the file is modified
+            // it will be correct loaded in the game. When the player changes the values using the
+            // options GUI, the values will be written in the file as well.
+            config = OptionsPanelHandler.Main.RegisterModOptions<Config>();
         }
 
     }
--- LukeMods.StorageInterest/Patches/StorageContainerPatches.cs
+++ LukeMods.StorageInterest/Patches/StorageContainerPatches.cs
@@ -18,6 +18,7 @@ namespace LukeMods.StorageInterest.Patches
     {
         static readonly Config C = Config.Instance;
         static readonly Dictionary<StorageContainer, float> InterestTime = new Dictionary<StorageContainer, float>();
+        static readonly Dictionary<StorageContainer, float> InterestTimeRequired = new Dictionary<StorageContainer, float>();
 
         static readonly FieldInfo itemGroups = AccessTools.Field(typeof(ItemsContainer), nameof(itemGroups));
 
@@ -33,19 +34,90 @@ namespace LukeMods.StorageInterest.Patches
             container.onAddItem += (_) => OnItemChanged(__instance);
             container.onRemoveItem += (_) => OnItemChanged(__instance);
 
+            // Start counting the interest as soon as the game loads.
             ResetTime(__instance);
+
+            // Avoid the need to open a container to start calculating the interest.
+            StartInterestRateCalculation(__instance);
+        }
+
+        /**
+         * Improve the player experience by giving visible feedback. 
+         * 
+         * This will allow the player to know the current state of
+         * each storage interest rate calculation.
+         */
+        [HarmonyPatch(nameof(OnHandHover)), HarmonyPostfix]
+        public static void OnHandHover(GUIHand hand)
+        {
+            GameObject target = hand.GetActiveTarget();
+            StorageContainer container = target.GetComponent<StorageContainer>();
+
+            // Assume all containers start empty.
+            string subscript = "Empty";
+            // If the container has items:
+            if (!container.IsEmpty())
+            {
+                // Get the time information to calculate the interest.
+                InterestTime.TryGetValue(container, out var timeIn);
+                InterestTimeRequired.TryGetValue(container, out var timeRequired);
+                var timePassed = Time.time - timeIn;
+
+                // By default, the interest is ready to be picked up!
+                // This is need as we must open the container manually to
+                // compute the interest and create the item.
+                string interestRate = "Ready!";
+
+                // There is an issue with the interest rate not starting
+                // properly. By checking if we have no target time, we can
+                // identify it and warn the player to take action.
+                // This may also be used when the container is full.
+                if (timeRequired == 0.0f)
+                {
+                    interestRate = "Open to start!";
+                }
+                // To improve the player experience, lets display the information
+                // about the current interest calculate progress.
+                else if (timePassed <= timeRequired)
+                {
+                    // Simply percentage calculation to help the user.
+                    int percentage = (int) ((timePassed / timeRequired) * 100);
+                    interestRate = percentage.ToString() + "%";
+                }
+                // Prefix the message to give more context to the player.
+                subscript = "Interest rate: " + interestRate;
+            }
+            // Add the message to the GUI when pointing the cursor to the storage.
+            HandReticle.main.SetText(HandReticle.TextType.HandSubscript, subscript, true, GameInput.Button.None);
         }
 
         [HarmonyPatch(nameof(Open)), HarmonyPrefix]
         public static void Open(StorageContainer __instance)
         {
+            // Whenever the container is open, trigger the interest rate calculation.
+            // This way we ensure that when ready the item will be computed and
+            // when in progress or not properly started, it will continue or start.
+            StartInterestRateCalculation(__instance);
+        }
+
+        /**
+         * Execute the actions to calculate and pay the interest.
+         */
+        private static void StartInterestRateCalculation(StorageContainer __instance)
+        {
             if (!InterestTime.TryGetValue(__instance, out var timeIn))
             {
-                return;
+                ResetTime(__instance);
             }
 
             var container = __instance.container;
             var counter = new Dictionary<TechType, int>();
+
+            // If the container is empty, there is no need to calculate the interest.
+            if (__instance.IsEmpty())
+            {
+                return;
+            }
 
             var maxCount = 0;
             var currTech = TechType.None;
@@ -61,11 +133,7 @@ namespace LukeMods.StorageInterest.Patches
                 }
             }
 
-            if (maxCount == 0)
-            {
-                return;
-            }
-
+            // If the container has no space for more items of this type, stop the interest.
             if (!container.HasRoomFor(currTech))
             {
                 return;
@@ -76,17 +144,24 @@ namespace LukeMods.StorageInterest.Patches
             var interestRate = C.InterestRate;
             var minTime = C.MinTime;
 
+            // Calculate the required time to compute the next interest payment.
             var timeRequired = Mathf.Max(interestRate / Mathf.Pow(2, maxCount - 1), minTime);
-            if (timeRequired > timePassed)
+            // Store the required time to use in the GUI.
+            InterestTimeRequired[__instance] = timeRequired;
+
+            // Do not create the item while we are still waiting for the required time.
+            if (timePassed < timeRequired)
             {
                 return;
             }
 
-            CoroutineHost.StartCoroutine(PickupItem(container, currTech));
+            // Calls the routine to create execute the payment.
+            CoroutineHost.StartCoroutine(PickupItem(__instance, container, currTech));
+            // Reset the time so we can iterate in the next interest calculation.
             ResetTime(__instance);
         }
 
-        private static IEnumerator PickupItem(ItemsContainer container, TechType techType)
+        private static IEnumerator PickupItem(StorageContainer __instance, ItemsContainer container, TechType techType)
         {
             var task = new TaskResult<Pickupable>();
             task.Set(null);
@@ -97,8 +172,23 @@ namespace LukeMods.StorageInterest.Patches
 
             if (pickupable != null)
             {
-                container.AddItem(pickupable);
-                pickupable.Pickup();
+                // Ensure we still have room for this item.
+                if (container.HasRoomFor(techType))
+                {
+                    container.AddItem(pickupable);
+                    pickupable.Pickup();
+                }
+
+                ResetTime(__instance);
+                // If we still have room for more:
+                if (container.HasRoomFor(techType))
+                {
+                    // Start the next interest calculation without the need of
+                    // the player closing the container and opening it again,
+                    // fixing the issue with some containers not properly
+                    // generating new items after it is done once.
+                    StartInterestRateCalculation(__instance);
+                }
             }
         }
 
@@ -123,16 +213,22 @@ namespace LukeMods.StorageInterest.Patches
             result.Set(pickupable);
         }
 
-        static void OnItemChanged(StorageContainer container)
+        static void OnItemChanged(StorageContainer __instance)
         {
-            ResetTime(container);
+            ResetTime(__instance);
+            // We need to restart the interest after we add or remove an item
+            // to the container. When we simply reset it, the interest rate
+            // calculation will break and won't restart without calling for it.
+            StartInterestRateCalculation(__instance);
         }
 
-        static void ResetTime(StorageContainer storage)
+        static void ResetTime(StorageContainer __instance)
         {
-            InterestTime[storage] = Time.time;
+            // Time used to check if the required time was fulfilled.
+            InterestTime[__instance] = Time.time;
+            // Initial value to handle the issue with containers not
+            // properly started.
+            InterestTimeRequired[__instance] = 0.0f;
         }
-
     }
-
 }
